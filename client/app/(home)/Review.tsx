1e6d0de9582d92755c5e562c6c1066d8c10e236b
susanodii <odiisusan@gmail.com>
2025-01-24 11:23:07 +0100

    fix typescript error message

--- client/app/(home)/Review.tsx
+++ client/app/(home)/Review.tsx
@@ -1,55 +1,64 @@
 import React from "react";
 import Image from "next/image";
 
-const Review = () => {
+interface ReviewDataProps {
+  data: {
+    count: number;
+    icon: string;
+    label: string;
+  };
+}
+
+const Review: React.FC = () => {
+  const reviewDetails = [
+    {
+      count: 45000,
+      icon: "/assets/images/Heart.png",
+
+      label: " Happy Customers",
+    },
+
+    {
+      count: 40000,
+      icon: "/assets/images/shirt-icon.png",
+
+      label: "  Products Sold",
+    },
+
+    {
+      count: 32146,
+      icon: "/assets/images/Phone.png",
+
+      label: "Contact Mobile",
+    },
+  ];
+
   return (
-    <main className='flex justify-around items-center  w-11/12 h-[191px] m-auto font-playfair bg-lightGray'>
+    // <div className='flex justify-around items-center  w-11/12 h-[191px] m-auto font-playfair bg-lightGray'>
+    <div className='flex flex-col gap-[2rem]  sm:flex-row justify-around items-center w-11/12 max-w-[1200px] min-h-[150px] sm:min-h-[191px] m-auto p-4 sm:p-6 font-playfair bg-lightGray sm:gap-4'>
+      {reviewDetails.map((data) => (
+        <ReviewData data={data} />
+      ))}
+    </div>
+  );
+};
+
+export default Review;
+
+const ReviewData: React.FC<ReviewDataProps> = (props) => {
+  const { data } = props;
+
+  return (
+    <main className=''>
       <div className='flex flex-col gap-2 items-center self-center '>
-        <Image
-          width={48}
-          height={48}
-          alt='logo-sticth'
-          src={"/assets/images/Heart.png"}
-        />
+        <Image width={48} height={48} alt='logo-sticth' src={data.icon} />
         <h2 className='font-medium text-[48px] leading-[60px] text-darkGray'>
-          45, 000
+          {data.count}
         </h2>
         <h5 className='text-[20px] font-medium leading-[28px] text-darkGray font-secondaryNormal'>
-          Happy Customers
-        </h5>
-      </div>
-
-      <div className='flex flex-col gap-2 items-center '>
-        <Image
-          width={48}
-          height={48}
-          alt='logo-sticth'
-          src={"/assets/images/shirt-icon.png"}
-        />
-        <h2 className='font-medium text-[48px] leading-[60px] text-darkGray font-playfair'>
-          40, 000
-        </h2>
-        <h5 className='text-[20px] font-medium leading-[28px] text-darkGray font-secondaryNormal'>
-          Products Sold
-        </h5>
-      </div>
-
-      <div className='flex flex-col gap-2 items-center '>
-        <Image
-          width={48}
-          height={48}
-          alt='logo-sticth'
-          src={"/assets/images/Phone.png"}
-        />
-        <h2 className='font-medium text-[48px] leading-[60px] text-darkGray font-playfair'>
-          32, 146
-        </h2>
-        <h5 className='text-[20px] font-medium leading-[28px] text-darkGray font-secondaryNormal'>
-          Contact Mobile
+          {data.label}
         </h5>
       </div>
     </main>
   );
 };
-
-export default Review;
