43235ce2392fbc80de671cb46d84e73007ae1622
susanodii <odiisusan@gmail.com>
2025-01-29 07:47:33 +0100

    resposive News section

--- client/app/(home)/TopRated.tsx
+++ client/app/(home)/TopRated.tsx
@@ -71,28 +71,31 @@ const TopRated = () => {
   ];
 
   return (
-    <main className=' mt-[4rem]  m-auto '>
-      <section className='w-full flex justify-between  items-center'>
+    <main className='mt-[4rem] w-11/12 mx-auto'>
+      {/* Header */}
+      <section className='w-full flex justify-between items-center'>
         <p className='font-playfair font-medium leading-[16px] text-[20px]'>
-          {" "}
-          Top Rated Designers{" "}
+          Top Rated Designers
         </p>
         <Button className='bg-accent text-primary rounded-3xl font-secondaryExraBold font-light leading-[19.2px] text-[16px]'>
           View All
         </Button>
       </section>
 
-      {/* card seco */}
-
-      <section className='flex justify-between mt-4'>
+      {/* Card Section */}
+      <section className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-4 justify-items-center'>
         {topRatedDetails.map((store, index) => (
-          <Card key={index} className='w-[400px]  '>
+          <Card
+            key={index}
+            className='w-full sm:w-[300px] md:w-[350px] lg:w-[400px]'
+          >
             <Image
               width={400}
               height={174}
-              alt='logo-sticth'
+              alt='logo-stitch'
               src={store.topRateImage}
             />
+
             <div className='flex justify-between mt-2 p-2'>
               <div className='flex flex-col gap-3'>
                 <CardHeader className='font-medium font-playfair leading-[15.85px] text-[16px] p-0'>
@@ -107,7 +110,7 @@ const TopRated = () => {
                     <Image
                       width={15}
                       height={15}
-                      alt='logo-sticth'
+                      alt='location-pin'
                       src={store.locationIcon}
                     />
                     <p className='font-secondaryNormal leading-[15px] text-[15px]'>
@@ -123,27 +126,24 @@ const TopRated = () => {
                 </p>
 
                 <div className='flex gap-8 mt-[3rem]'>
-                  {" "}
-                  <Button className=' bg-primary text-accent rounded-3xl w-[100px] border-primary border font-secondaryExraBold font-semibold leading-[24px]'>
+                  <Button className='bg-primary text-accent rounded-3xl w-full sm:w-[100px] border-primary border font-secondaryExraBold font-semibold leading-[24px]'>
                     Chat
                   </Button>
-                  <Button className='bg-accent text-primary rounded-3xl  w-[100px] font-secondaryExraBold font-semibold leading-[24px]'>
+                  <Button className='bg-accent text-primary rounded-3xl w-full sm:w-[100px] font-secondaryExraBold font-semibold leading-[24px]'>
                     Book
                   </Button>
                 </div>
               </div>
+
               <div className='flex gap-2'>
                 <div className='h-[24px] w-[24px] cursor-pointer'>
                   <Image
-                    // width={12}
-                    // height={15.4}
                     width={20}
                     height={20}
                     alt='bookmark'
                     src={store.bookMarkIcon}
                   />
                 </div>
-
                 <div className='h-[24px] w-[24px] cursor-pointer'>
                   <Image
                     width={20}
@@ -158,6 +158,94 @@ const TopRated = () => {
         ))}
       </section>
     </main>
+
+    // <main className=' mt-[4rem]  m-auto '>
+    //   <section className='w-full flex justify-between  items-center'>
+    //     <p className='font-playfair font-medium leading-[16px] text-[20px]'>
+    //       {" "}
+    //       Top Rated Designers{" "}
+    //     </p>
+    //     <Button className='bg-accent text-primary rounded-3xl font-secondaryExraBold font-light leading-[19.2px] text-[16px]'>
+    //       View All
+    //     </Button>
+    //   </section>
+
+    //   {/* card seco */}
+
+    //   <section className='flex justify-between mt-4'>
+    //     {topRatedDetails.map((store, index) => (
+    //       <Card key={index} className='w-[400px]  '>
+    //         <Image
+    //           width={400}
+    //           height={174}
+    //           alt='logo-sticth'
+    //           src={store.topRateImage}
+    //         />
+    //         <div className='flex justify-between mt-2 p-2'>
+    //           <div className='flex flex-col gap-3'>
+    //             <CardHeader className='font-medium font-playfair leading-[15.85px] text-[16px] p-0'>
+    //               {store.storeName}
+    //             </CardHeader>
+    //             <CardContent className='font-secondaryNormal font-normal leading-[17px] p-0 text-[14px]'>
+    //               {store.storeDetails}
+    //             </CardContent>
+
+    //             <div className='flex gap-6 items-center'>
+    //               <div className='flex gap-1 items-center'>
+    //                 <Image
+    //                   width={15}
+    //                   height={15}
+    //                   alt='logo-sticth'
+    //                   src={store.locationIcon}
+    //                 />
+    //                 <p className='font-secondaryNormal leading-[15px] text-[15px]'>
+    //                   {store.location}
+    //                 </p>
+    //               </div>
+    //               <p className='font-secondaryNormal leading-[15px] text-[15px] cursor-pointer'>
+    //                 {store.map}
+    //               </p>
+    //             </div>
+    //             <p className='font-secondaryNormal leading-[8px] text-gray'>
+    //               {store.mapDetails}
+    //             </p>
+
+    //             <div className='flex gap-8 mt-[3rem]'>
+    //               {" "}
+    //               <Button className=' bg-primary text-accent rounded-3xl w-[100px] border-primary border font-secondaryExraBold font-semibold leading-[24px]'>
+    //                 Chat
+    //               </Button>
+    //               <Button className='bg-accent text-primary rounded-3xl  w-[100px] font-secondaryExraBold font-semibold leading-[24px]'>
+    //                 Book
+    //               </Button>
+    //             </div>
+    //           </div>
+    //           <div className='flex gap-2'>
+    //             <div className='h-[24px] w-[24px] cursor-pointer'>
+    //               <Image
+    //                 // width={12}
+    //                 // height={15.4}
+    //                 width={20}
+    //                 height={20}
+    //                 alt='bookmark'
+    //                 src={store.bookMarkIcon}
+    //               />
+    //             </div>
+
+    //             <div className='h-[24px] w-[24px] cursor-pointer'>
+    //               <Image
+    //                 width={20}
+    //                 height={20}
+    //                 alt='share'
+    //                 src={store.shareIcon}
+    //               />
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </Card>
+    //     ))}
+    //   </section>
+    // </main>
   );
 };
 
