4d0b054e0fa01bca4b1811faea04e147bffe6e54
susanodii <odiisusan@gmail.com>
2025-02-26 13:42:19 +0100

    responsive nav bar

--- client/components/layouts/Header.tsx
+++ client/components/layouts/Header.tsx
@@ -1,30 +1,103 @@
+"use client";
+
 import { headerLinks } from "@/lib/constants";
+
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { FiShoppingCart } from "react-icons/fi";
 import { Button } from "../ui/button";
+import { useMediaQuery } from "@/hooks/use-mediaQuery";
+import { MdOutlineMenu } from "react-icons/md";
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
 function Header() {
+  const isDesktop = useMediaQuery("(min-width:768px)");
   return (
-    <nav className='w-full h-[84px] bg-white flex justify-center items-center sticky top-0'>
-        <section className='max-w-[1440px] w-full flex justify-between items-center'>
-           
-
-           
-            <Image priority width={140} height={140} alt='logo-sticth' src={'/assets/images/stitch-logo.svg'} />
+    <>
+      {isDesktop ? (
+        <nav className='w-full h-[84px] bg-white flex justify-center items-center sticky top-0'>
+          <section className='max-w-[1440px] w-full flex justify-between items-center'>
+            <Image
+              priority
+              width={140}
+              height={140}
+              alt='logo-sticth'
+              src={"/assets/images/stitch-logo.svg"}
+            />
             <div className='flex gap-8 items-center font-medium '>
-                {headerLinks.map((link, idx)=> <Link className='hover:border-b-primary hover:border-b-2' key={idx} href={link.url}>{link.label}</Link>)}
+              {headerLinks.map((link, idx) => (
+                <Link
+                  className='hover:border-b-primary hover:border-b-2'
+                  key={idx}
+                  href={link.url}
+                >
+                  {link.label}
+                </Link>
+              ))}
             </div>
             <div className='flex gap-2 items-center'>
-            <FiShoppingCart className='text-primary' size={24} />
-            <Button className='bg-primary text-white rounded-3xl'>Register</Button>
-            <Button className='bg-accent text-primary rounded-3xl border-primary border'>Login</Button>
+              <FiShoppingCart className='text-primary' size={24} />
+              <Button className='bg-primary text-white rounded-3xl'>
+                Register
+              </Button>
+              <Button className='bg-accent text-primary rounded-3xl border-primary border'>
+                Login
+              </Button>
             </div>
-            
-        </section>
-         
-    </nav>
+          </section>
+        </nav>
+      ) : (
+        <nav>
+          <Drawer direction='right'>
+            <DrawerTrigger asChild>
+              <div className='py-2 flex justify-between items-center px-2'>
+                <Image
+                  priority
+                  width={140}
+                  height={140}
+                  alt='logo-sticth'
+                  src={"/assets/images/stitch-logo.svg"}
+                />
+                <MdOutlineMenu size={30} />
+              </div>
+            </DrawerTrigger>
+            <DrawerContent className='py-4'>
+              <div className='flex flex-col gap-8  font-medium  px-6 '>
+                {headerLinks.map((link, idx) => (
+                  <Link
+                    className='hover:border-b-primary hover:border-b-2'
+                    key={idx}
+                    href={link.url}
+                  >
+                    {link.label}
+                  </Link>
+                ))}
+              </div>
+              <div className='flex flex-col gap-2 px-6 mt-8'>
+                {/* <FiShoppingCart className='text-primary' size={24} /> */}
+                <Button className='bg-primary text-white rounded-3xl'>
+                  Register
+                </Button>
+                <Button className='bg-accent text-primary rounded-3xl border-primary border'>
+                  Login
+                </Button>
+              </div>
+            </DrawerContent>
+          </Drawer>
+        </nav>
+      )}
+    </>
   );
 }
 
--- client/components/ui/drawer.tsx
+++ client/components/ui/drawer.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Drawer as DrawerPrimitive } from "vaul"
+import * as React from "react";
+import { Drawer as DrawerPrimitive } from "vaul";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const Drawer = ({
   shouldScaleBackground = true,
@@ -13,14 +13,14 @@ const Drawer = ({
     shouldScaleBackground={shouldScaleBackground}
     {...props}
   />
-)
-Drawer.displayName = "Drawer"
+);
+Drawer.displayName = "Drawer";
 
-const DrawerTrigger = DrawerPrimitive.Trigger
+const DrawerTrigger = DrawerPrimitive.Trigger;
 
-const DrawerPortal = DrawerPrimitive.Portal
+const DrawerPortal = DrawerPrimitive.Portal;
 
-const DrawerClose = DrawerPrimitive.Close
+const DrawerClose = DrawerPrimitive.Close;
 
 const DrawerOverlay = React.forwardRef<
   React.ElementRef<typeof DrawerPrimitive.Overlay>,
@@ -31,8 +31,8 @@ const DrawerOverlay = React.forwardRef<
     className={cn("fixed inset-0 z-50 bg-black/80", className)}
     {...props}
   />
-))
-DrawerOverlay.displayName = DrawerPrimitive.Overlay.displayName
+));
+DrawerOverlay.displayName = DrawerPrimitive.Overlay.displayName;
 
 const DrawerContent = React.forwardRef<
   React.ElementRef<typeof DrawerPrimitive.Content>,
@@ -43,18 +43,18 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-y-0 left-0 z-50 flex w-64 flex-col bg-background",
+        "fixed inset-y-0 left-20  z-50 flex w-64 max-w-80 flex-col bg-background",
         // "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
         className
       )}
       {...props}
     >
-      <div className="" />
+      <div className='' />
       {children}
     </DrawerPrimitive.Content>
   </DrawerPortal>
-))
-DrawerContent.displayName = "DrawerContent"
+));
+DrawerContent.displayName = "DrawerContent";
 
 const DrawerHeader = ({
   className,
@@ -64,8 +64,8 @@ const DrawerHeader = ({
     className={cn("grid gap-1.5 p-4 text-center sm:text-left", className)}
     {...props}
   />
-)
-DrawerHeader.displayName = "DrawerHeader"
+);
+DrawerHeader.displayName = "DrawerHeader";
 
 const DrawerFooter = ({
   className,
@@ -75,8 +75,8 @@ const DrawerFooter = ({
     className={cn("mt-auto flex flex-col gap-2 p-4", className)}
     {...props}
   />
-)
-DrawerFooter.displayName = "DrawerFooter"
+);
+DrawerFooter.displayName = "DrawerFooter";
 
 const DrawerTitle = React.forwardRef<
   React.ElementRef<typeof DrawerPrimitive.Title>,
@@ -90,8 +90,8 @@ const DrawerTitle = React.forwardRef<
     )}
     {...props}
   />
-))
-DrawerTitle.displayName = DrawerPrimitive.Title.displayName
+));
+DrawerTitle.displayName = DrawerPrimitive.Title.displayName;
 
 const DrawerDescription = React.forwardRef<
   React.ElementRef<typeof DrawerPrimitive.Description>,
@@ -102,8 +102,8 @@ const DrawerDescription = React.forwardRef<
     className={cn("text-sm text-muted-foreground", className)}
     {...props}
   />
-))
-DrawerDescription.displayName = DrawerPrimitive.Description.displayName
+));
+DrawerDescription.displayName = DrawerPrimitive.Description.displayName;
 
 export {
   Drawer,
@@ -116,4 +116,4 @@ export {
   DrawerFooter,
   DrawerTitle,
   DrawerDescription,
-}
+};
