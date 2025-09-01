5cec1c8c7af6d1288cc1a432eb239f2ac6a664dc
susanodii <you@example.com>
2025-09-01 14:03:35 +0100

    sign up

--- client/app/(public)/(auth)/sign-in/SignInPage.tsx
+++ client/app/(public)/(auth)/sign-in/SignInPage.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import React from "react";
@@ -5,7 +6,9 @@ import Image from "next/image";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { Checkbox } from "@/components/ui/checkbox"
+import { Label } from "@/components/ui/label"
+
 
 import { Button } from "@/components/ui/button";
 import {
@@ -18,10 +21,11 @@ import {
   FormMessage,
 } from "@/components/forminputs/CustomFormInput";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 const SignInPage = () => {
   return (
-    <section className='flex  h-lvh justify-around  '>
+    <section className='flex  h-l00vh   w-full '>
       <div className='hidden md:block w-1/2 bg-[#ECECE0] py-[3rem] px-[3rem]'>
        
         <p className='text-2xl text-[#245949] mt-[2rem] '>
@@ -42,20 +46,23 @@ const SignInPage = () => {
       </div>
 
       {/* form */}
-      <div className=" bg-white self-center">
+      <div className=" bg-white  py-[1rem] flex  flex-col justify-center items-center m-auto w-full md:w-1/2   ">
 
-         <div>
+         <div className="flex justify-center items-center mb-[2rem]">
               <Image
                 priority
-                width={30}
-                height={30}
+                width={70}
+                height={70}
                 alt='hero-image'
                 src='/assets/images/logo1.png'
                 className=''
               />
+
+            
             </div>
 
-             <div className='  flex justify-center items-center m-auto  text-center '>
+             {/* <div className='  flex justify-center items-center m-auto  text-center '> */}
+              <div className="w-full md:w-3/4 lg:w-1/2 px-4">
         <SignUpForm />
       </div>
       </div>
@@ -70,23 +77,32 @@ export function SignUpForm() {
   const form = useForm();
 
   return (
-    <Form {...form}>
-      <div className='   '>
-        <div className=' bg-white '>
+      <div className='    w-full   py-[2rem] '>
+            <p className='text-2xl '>Sign In </p>
+            
+        <p className="mb-[2rem]">
+          Don't have an account?
+
+<Link href="/sign-up" className="text-blue-500 hover:underline ml-1"> 
+Sign up
+</Link>
+        </p>
         
+    <Form {...form}>
+      
+         
 
           {/* form */}
 
           <form className=''>
 
-             <p className='text-2xl '>Sign In </p>
             
             {/* Email */}
             <FormField
               control={form.control}
               name='email'
               render={({ field }) => (
-                <FormItem className='flex flex-col items-start'>
+                <FormItem className='flex flex-col items-start w-full mb-4'>
                   <FormLabel className='text-[#666666] text-sm'>
                     Email
                   </FormLabel>
@@ -105,8 +121,8 @@ export function SignUpForm() {
               control={form.control}
               name='password'
               render={({ field }) => (
-                <FormItem className='md:col-span-1'>
-                  <FormLabel className='flex justify-between'>
+                <FormItem className=''>
+                  <FormLabel className='flex justify-between '>
                     <p className='text-[#666666] text-sm'>Password</p>
                     <div className='flex gap-2 items-center'>
                       <Image
@@ -124,23 +140,45 @@ export function SignUpForm() {
                       type='password'
                       placeholder='Password'
                       {...field}
-                      className='w-full'
+                      className='w-full mb-4'
                     />
                   </FormControl>
+                  <div className="flex justify-between w-full items-center mt-[1rem] ">
+
+                    
+                         <div className="flex items-center gap-3">
+        <Checkbox id="terms" />
+        <Label htmlFor="terms">Remember Me</Label>
+      </div>
+
+                    <Link href="/reset-password" className="text-blue-500 hover:underline text-sm">
+                      Forgot Password?
+                    </Link>
+
+                  </div>
+            
                 </FormItem>
               )}
             />
 
            
             {/* Submit Button */}
-            <div className='flex items-center gap-2 md:col-span-2  '>
-                 <div>
-                <Button type='submit'>Sign In</Button>
-              </div>
+            <div className='flex items-center w-full mt-4 '>
+                 
+                <Button  size='lg' className="w-full" >Sign In</Button>
+              
             </div>
           </form>
-        </div>
-      </div>
+      
     </Form>
+        </div>
   );
 }
+
+
+
+
+
+
+
+
