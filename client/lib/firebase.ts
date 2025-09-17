2f82f682c393c0406872426e8c489865a4bdc3bc
susanodii <you@example.com>
2025-09-17 07:16:27 +0100

    fix: hide firebase secret keys in env file

--- client/lib/firebase.ts
+++ client/lib/firebase.ts
@@ -1,20 +1,22 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAUpkeO73L8JJ5ZOy7ZQu3vxrOty7w6CZ8",
-  authDomain: "stich-d3357.firebaseapp.com",
-  projectId: "stich-d3357",
-  storageBucket: "stich-d3357.firebasestorage.app",
-  messagingSenderId: "943316088581",
-  appId: "1:943316088581:web:2510dd69d0f446e6e0696c",
-  measurementId: "G-8KZP67LZ4Q"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId:  process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId:  process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId:  process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
